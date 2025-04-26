98363da8b108cd4fbe3ec441b245a5e15b33d04a
Facundo Sura <facundomesura@gmail.com>
2025-04-26 14:47:08 -0300

    footer y navbar listos

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,24 +1,44 @@
+import { useState } from "react";
 import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 
 function Navbar({ handleChange, handleSubmit }) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <nav className={styles.container}>
-      <Link className={styles.link} to="/">
-        PI-PAISES
-      </Link>
-      <Link className={styles.link} to="/countries">
-        INICIO
-      </Link>
-      <Link className={styles.link} to="/countries/form">
-        CREAR ACTIVIDAD
-      </Link>
-      <form className={styles.form} onChange={handleChange}>
-        <input className={styles.input} placeholder="País" type="search" />
-        <button className={styles.button} type="submit" onClick={handleSubmit}>
-          BUSCAR
-        </button>
-      </form>
+      <div className={styles.logo}>
+        <form className={styles.form} onChange={handleChange}>
+          <input className={styles.input} placeholder="País" type="search" />
+          <button
+            className={styles.button}
+            type="submit"
+            onClick={handleSubmit}>
+            BUSCAR
+          </button>
+        </form>
+        <div className={styles.hamburger} onClick={toggleMenu}>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+          <span className={styles.bar}></span>
+        </div>
+      </div>
+
+      <div className={`${styles.menu} ${isOpen ? styles.open : ""}`}>
+        <Link className={styles.link} to="/">
+          PI-PAISES
+        </Link>
+        <Link className={styles.link} to="/countries">
+          INICIO
+        </Link>
+        <Link className={styles.link} to="/countries/form">
+          CREAR ACTIVIDAD
+        </Link>
+      </div>
     </nav>
   );
 }
