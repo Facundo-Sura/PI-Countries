ce6a288c87932b17d7d1da55bbf4e4a156068204
Facundo Sura <facundomesura@gmail.com>
2025-04-26 02:26:13 -0300

    modificaciones en el Footer y toda la navbar estructurada

--- client/src/components/navbar/navbar.jsx
+++ client/src/components/navbar/navbar.jsx
@@ -1,26 +1,25 @@
+import styles from "./Navbar.module.css";
 import { Link } from "react-router-dom";
 
 function Navbar({ handleChange, handleSubmit }) {
   return (
-    <div className="navbar navbar-expand-lg bg-body-tertiary z-3">
-      <div className="container-fluid">
-        <Link className="text-decoration-none" to="/">
-          <h1 className="navbar-brand">PI-PAISES</h1>
-        </Link>
-        <Link className="text-decoration-none" to="/countries">
-          <h4 className="nav-link">INICIO</h4>
-        </Link>
-        <Link className="text-decoration-none" to="/countries/form">
-          <h4 className="nav-link">CREAR ACTIVIDAD</h4>
-        </Link>
-        <form className="d-flex" onChange={handleChange}>
-          <input className="form-control me-2" placeholder="País" type="search" />
-          <button className="btn btn-outline-secondary" type="submit" onClick={handleSubmit}>
-            BUSCAR
-          </button>
-        </form>
-      </div>
-    </div>
+    <nav className={styles.container}>
+      <Link className={styles.link} to="/">
+        PI-PAISES
+      </Link>
+      <Link className={styles.link} to="/countries">
+        INICIO
+      </Link>
+      <Link className={styles.link} to="/countries/form">
+        CREAR ACTIVIDAD
+      </Link>
+      <form className={styles.form} onChange={handleChange}>
+        <input className={styles.input} placeholder="País" type="search" />
+        <button className={styles.button} type="submit" onClick={handleSubmit}>
+          BUSCAR
+        </button>
+      </form>
+    </nav>
   );
 }
 
